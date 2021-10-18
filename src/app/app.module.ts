3b49b6c4661f2fe0818abe06cbd8fbef95fca10f
Dave <david.mathaga@student.moringaschool.com>
2021-10-18 19:22:44 +0300

    two-way binding search form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserComponent } from './components/user/user.component';
 import { RepositoryComponent } from './components/repository/repository.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { RepositoryComponent } from './components/repository/repository.componen
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
