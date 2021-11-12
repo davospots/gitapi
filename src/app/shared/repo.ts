211abea86b85137804b281e9f73d25af6bfbaeb3
Dave <david.mathaga@student.moringaschool.com>
2021-11-12 05:50:18 +0300

    removing redundant files

--- src/app/shared/repo.ts
+++
@@ -1,8 +0,0 @@
-export class Repo {
-    constructor(
-        public name: string,
-        public description: string,
-        public created_at: Date,
-        public html_url: string,
-      ) {}
-}
